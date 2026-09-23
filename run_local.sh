2e6fe2894714aa7433ed3da88d0befc9e3a1e270
cloud155 <96786154+cloud155@users.noreply.github.com>
2026-09-23 23:48:59 +0500

    Delete run_local.sh

--- run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run_local.sh
-set -e
-
-echo "Сборка Docker-образа..."
-docker build -t immortalwrt-builder .
-
-echo "Запуск сборки прошивки..."
-docker run --rm -v "$(pwd):/home/build/workspace:z" -w /home/build/workspace immortalwrt-builder ./build.sh
